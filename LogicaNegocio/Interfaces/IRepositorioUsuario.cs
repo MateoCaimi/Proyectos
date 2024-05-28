930ef491d52e9494a5a54dea5288f38aa1e4378d
Agustin Carretto Zorrilla de San Martin <agus.carretto@gmail.com>
2024-05-27 23:33:38 -0300

    Session inicio y cierre de sesion

--- LogicaNegocio/Interfaces/IRepositorioUsuario.cs
+++ LogicaNegocio/Interfaces/IRepositorioUsuario.cs
@@ -9,6 +9,20 @@ namespace LogicaNegocio.Interfaces
 {
     public interface IRepositorioUsuario : IRepositorio<Usuario>
     {
+        public void InicioSesion(Usuario u);
+        public Usuario UsuarioPorNombreUsuario(string nombreUsuario);
+
+        public Usuario Buscar(int id);
+
+        public bool TieneObrasAbiertas(Usuario item);
+
+        public IEnumerable<Usuario> TomarTodos();
+
+        public IEnumerable<Usuario> TomarTodosDeObra();
+
+        public IEnumerable<Usuario> TomarTodosDeOficina();
+
+        public IEnumerable<Usuario> TomarTodosDeNormal();
 
     }
 }
