fd979552b2e40fc43050822b3229df6ced60c5d5
Agustin Carretto Zorrilla de San Martin <agus.carretto@gmail.com>
2024-07-30 18:39:53 -0300

    dise;o

--- MVC/wwwroot/js/site.js
+++ MVC/wwwroot/js/site.js
@@ -2,3 +2,25 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+function mostrarOcultar() {
+    let layout = document.getElementById('layout');
+    let capa = document.getElementById('capaDeFondo');
+    let layoutStyle = window.getComputedStyle(layout);
+   // let capaStyle = window.getComputedStyle(capa);
+
+
+    if (layoutStyle.getPropertyValue('left') == "-250px") {
+        capa.style.setProperty('opacity', '0.5')
+        capa.style.setProperty('z-index', '0');
+        layout.style.setProperty('left', '0px');
+
+    } else {
+        layout.style.setProperty('left', '-250px');
+        capa.style.setProperty('opacity', '0')
+        capa.style.setProperty('z-index', '-10');
+
+    }
+     
+}
